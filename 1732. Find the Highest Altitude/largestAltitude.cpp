e3d614a14e78237234f0c7417846279cc868c94f
TkieuSU <150384059+TkieuSU@users.noreply.github.com>
2024-09-05 15:49:21 -0700

    Update largestAltitude.cpp

--- 1732. Find the Highest Altitude/largestAltitude.cpp
+++ 1732. Find the Highest Altitude/largestAltitude.cpp
@@ -3,8 +3,11 @@ public:
     int largestAltitude(vector<int>& gain) {
         int altitude = 0;
         int maxAltitude = 0;
+
         for (int i = 0; i < gain.size(); i++) {
+            // Keep track of the altitude gain or loss
             altitude += gain[i];
+            // Compare and keep track of the max altitude gain
             maxAltitude = max(altitude, maxAltitude);
         }
         return maxAltitude;
